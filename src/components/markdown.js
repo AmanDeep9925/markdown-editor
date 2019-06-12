b5e8f5b910cb59f1f7e7b6a3d11cb94d7cdb079f
AmanDeep9925 <amandeep58257@gmail.com>
2019-06-12 20:03:15 +0530

    Markdown Component

---
+++ src/components/markdown.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Remarkable from "remarkable";
+
+
+class markdown extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      value: "Start your Markdown's here!"
+    };
+  }
+  handleChange(e) {
+    this.setState({
+      value: e.target.value
+    });
+  }
+  getRawMarkup() {
+    const md = new Remarkable();
+    return { __html: md.render(this.state.value) };
+  }
+  render() {
+    return (
+      <div className='container'>
+        <div className='input'>
+          <h3>Input</h3>
+          <textarea
+            className='input-text'
+            onChange={this.handleChange}
+            defaultValue={this.state.value}
+          />
+        </div>
+        <div className="output">
+            <h3>MarkDown</h3>
+            <div dangerouslySetInnerHTML={this.getRawMarkup()}
+                    className="output-text">
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default markdown;
